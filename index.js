c741b13d83fdd6e2b2c3b6ac58d8cbce666782ce
shekhar sawarni <shekharsawarni@gmail.com>
2026-01-02 11:32:02 +0530

    Revert "added isabove18"
    This reverts commit 91819696484ab38acd1efdb8ea11c7720415b31e.

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const age=24;
-const name1="piyush garg";
+const name="piyush garg";
 
 const db='01/01/2000';
 
 function sayName(){
-    console.log("say my name"+ name1);
-}
-
-function isabove18(){
-    return age>=18;
+    console.log("say my name"+ name);
 }
